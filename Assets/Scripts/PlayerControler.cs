626ea2a7eb72a56834a880594122770e981d7ed6
vavytas <43293957+vavytas@users.noreply.github.com>
2026-01-30 22:14:57 +0200

    Basic moving around

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
+    [SerializeField] public Rigidbody2D BodyPlayer;
+    [SerializeField] public float Speed = 350f;
+    private Vector2 moveInput;
+    private float horizontal;
+    private float vertical;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +20,12 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        BodyPlayer.linearVelocity = moveInput * Speed;
+    }
+
+
+public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
