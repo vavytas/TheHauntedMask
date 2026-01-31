baff2f3d4d5e40c8d1c39451136171a45635a623
vavytas <43293957+vavytas@users.noreply.github.com>
2026-01-31 10:27:24 +0200

    ghost animations and follow

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -7,8 +7,10 @@ public class EnemyFollow : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private GameObject basePlace;
 
-
-    public float speed = 2f;
+    [SerializeField] private Transform[] dummyTargets;
+    [SerializeField] private float darknessIntensity = 0.4f;
+    public bool targetPlayer = true;
+    [SerializeField] public float speed = 2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,9 +22,12 @@ public class EnemyFollow : MonoBehaviour
     void Update()
     {
         float distance = Vector3.Distance(transform.position, target.position);
-        if (distance > 0.1f)
+        if (distance > 1f)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            Vector3 diff = (target.transform.position - transform.position);
+            float atan2 = Mathf.Atan2(diff.y, diff.x);
+            transform.rotation = Quaternion.Euler(0f, 0f, atan2 * Mathf.Rad2Deg - 90f);
         }
         else
         {
@@ -37,9 +42,15 @@ public class EnemyFollow : MonoBehaviour
     public IEnumerator GoDarkByFive()
     {
         MainManager.mainManager.goDark = false;
-        MainManager.mainManager.globalLight.intensity -= 0.2f;
-       yield return new WaitForSeconds(0.5f);
+        MainManager.mainManager.globalLight.intensity -= darknessIntensity;
+        int tar = Random.Range(0, dummyTargets.Length);
+        speed = 5f;
+        target = dummyTargets[tar];
+        yield return new WaitForSeconds(0.5f);
         MainManager.mainManager.goDark = true;
+        yield return new WaitForSeconds(2.5f);
+        target = player.transform;
+        speed = 2f;
     }
 
 }
